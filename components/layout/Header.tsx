a833307ef99e2543ce663ffec253ceb9aaf2e192
Hellshi <hcslimaa@gmail.com>
2021-08-03 10:01:02 -0300

    style: styled header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,13 +1,27 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Header } from 'react-native-elements';
+import colors from '../../utils';
 
-const Header = () => (
-  <View>
-    <Text> AAAAA </Text>
-  </View>
+const { PRIMARY_COLOR } = colors;
+
+const HeaderComponent = () => (
+  <Header
+    containerStyle={{ height: 100, alignContent: 'center', justifyContent: 'center' }}
+    backgroundColor="#fff"
+    placement="left"
+    centerComponent={{
+      text: 'TGL',
+      style: {
+        color: '#707070', borderBottomColor: PRIMARY_COLOR, borderBottomWidth: 3, fontSize: 25, fontWeight: 'bold',
+      },
+    }}
+    rightComponent={{ icon: 'logout', color: '#C1C1C1' }}
+  />
 );
 
 const styles = StyleSheet.create({});
 
-export default Header;
+export default HeaderComponent;
